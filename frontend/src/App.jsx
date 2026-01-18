655256fa7e741acff8c4f299ba7c202017deba96
saradir <120840462+saradir@users.noreply.github.com>
2026-01-18 19:12:32 +0200

    add browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import Homepage from './components/Homepage'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
 import './App.css'
+import Leaderboard from './components/Leaderboard'
 
 function App() {
 
 
   return (
     <>
-      <Homepage />
+    <Routes>
+        <Route
+          path="/"
+          element={<Homepage />}
+        />
+
+        <Route
+          path="/leaderboard"
+          element={<Leaderboard />}
+        />
+     </Routes>   
     </>
   )
 }
